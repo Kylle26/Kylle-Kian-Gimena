e83c8ed4d63d6ef71f3dbc136404e6a063da4148
Kylle26 <Gimenakylle26@gmail.com>
2025-09-21 20:33:31 +0800

    Add files via upload
    Logo Making using C++

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std; 
+int main() {
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC                 \n";
+    cout << "CCCC             |             |             \n";
+    cout << "CCCC             |             |             \n";
+    cout << "CCCC         ____|____     ____|____         \n";
+    cout << "CCCC             |             |             \n";
+    cout << "CCCC             |             |             \n";
+    cout << "CCCC             |             |             \n";
+    cout << "CCCC                \n";
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    cout << "CCCCCCCCCCCCCCCCCCCC\n";
+    
+    
+}
